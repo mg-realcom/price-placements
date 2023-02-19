c41e36456d0db978ea4c6a8db21421f931fb5d63
viktor <vi.dave@yandex.ru>
2023-02-19 07:26:56 +0300

    [fix] Fix prepare response

--- response.go
+++ response.go
@@ -7,11 +7,12 @@ import (
 
 func GetResponse(url string) (response *http.Response, err error) {
 	response, err = http.Get(url)
-	if response.StatusCode != 200 {
-		return response, fmt.Errorf("feed not availible. Status:%s", response.Status)
-	}
 	if err != nil {
 		return response, fmt.Errorf("can't get feed. Error:%w", err)
 	}
+	if response.StatusCode != 200 {
+		return response, fmt.Errorf("feed not availible. Status:%s", response.Status)
+	}
+
 	return response, err
 }
